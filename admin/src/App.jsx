52a5661884d723aa1ebd26280af0eb959279a458
ARIJIT SOAN <arijitsoan@gmail.com>
2025-02-11 09:15:39 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:8000"
+  const url = "https://food-del-backend-jxug.onrender.com"
 
   return (
     <div>
